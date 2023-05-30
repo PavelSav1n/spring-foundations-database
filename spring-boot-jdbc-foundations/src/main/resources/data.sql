505d179e05ec6a97620105b1a848d7392682b0d1
Pavel <savin.pavel.a@gmail.com>
2023-05-31 00:17:36 +0400

    Initial commit

---
+++ spring-boot-jdbc-foundations/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO students(id , fio, age) VALUES (1, 'Ivanov', 20), (2, 'Petrov', 23),(3, 'Sidorov', 33),(4, 'Semenov', 25);
---
+++ spring-boot-jdbc-homework/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(id , name, age) VALUES
+    (1, 'Peter Parker', 25),
+    (2, 'Batman', 41),
+    (3, 'Martie McFly', 33),
+    (4, 'Emmet Brown', 55);
