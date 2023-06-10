8325b941192e6f458384c322df8ec83382a2f3fe
Pavel <savin.pavel.a@gmail.com>
2023-06-10 23:13:17 +0400

    Added to jpa practice: 1. Genre & Place domains 2. data.sql 3. schema.sql 4. properties into yaml-file to manage creation of h2 tables (creating tables and filling data with JPA or Hibenate means)

--- spring-boot-jpa-foundations/src/main/java/ru/itsjava/domain/Film.java
+++ spring-boot-jpa-foundations/src/main/java/ru/itsjava/domain/Film.java
@@ -1,15 +1,23 @@
 package ru.itsjava.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity(name = "Films")
 public class Film {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long id1;
+
     private String title;
+
+    @ManyToOne(targetEntity = Genre.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "genre_id") // название колонки в этой сущности (Film)
+    private Genre genre;
+
+    @OneToMany(targetEntity = Place.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "film_id") // название колонки в сущности Place
+    private List<Place> places;
 }
--- spring-boot-jpa-foundations/src/main/resources/data.sql
+++ spring-boot-jpa-foundations/src/main/resources/data.sql
@@ -1,3 +1,12 @@
-INSERT INTO films (title) VALUES
-('Harry Potter'),
-('Star Wars');
+INSERT INTO genre (name) VALUES
+('fantasy'),
+('comedy');
+
+INSERT INTO films (title, genre_id) VALUES
+('Harry Potter', 1),
+('Star Wars', 2);
+
+INSERT INTO place (name, film_id) VALUES
+('London', 1),
+('Train', 1),
+('Space', 2);
