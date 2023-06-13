616b5fa69f363ae05b805df16c1a63e2c36fe696
Pavel <savin.pavel.a@gmail.com>
2023-06-13 21:07:50 +0400

    spring-boot-jpa-homework initial commit

---
+++ spring-boot-jpa-homework/src/main/java/ru/itsjava/SpringBootJpaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.itsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHomeworkApplication.class, args);
+	}
+
+}
